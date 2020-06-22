918402e5546645af69bcb606ec3d943cc864e069
tichise <ichise@gmail.com>
2020-06-22 14:39:22 +0900

    target SampleWatchKitAppを追加

---
+++ Sample/SampleWatchKitApp Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  SampleWatchKitApp Extension
+//
+//  Created by tichise on 2020年6月22日 20/06/22.
+//  Copyright © 2020 ichise. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ Sample/SampleWatchKitApp Extension/NotificationController.swift
@@ -0,0 +1,34 @@
+//
+//  NotificationController.swift
+//  SampleWatchKitApp Extension
+//
+//  Created by tichise on 2020年6月22日 20/06/22.
+//  Copyright © 2020 ichise. All rights reserved.
+//
+
+import WatchKit
+import SwiftUI
+import UserNotifications
+
+class NotificationController: WKUserNotificationHostingController<NotificationView> {
+
+    override var body: NotificationView {
+        return NotificationView()
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
