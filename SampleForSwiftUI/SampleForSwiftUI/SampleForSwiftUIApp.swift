ba46a633625a4d1f171e15a6268d6fa6953d6d12
tichise <43707+tichise@users.noreply.github.com>
2021-07-25 15:41:59 +0900

    new

---
+++ SampleForSwiftUI/SampleForSwiftUI/SampleForSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleForSwiftUIApp.swift
+//  SampleForSwiftUI
+//
+//  Created by tichise on 2021年7月25日 21/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleForSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
